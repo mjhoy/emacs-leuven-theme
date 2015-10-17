2e407757d4a042513e07c9f6aada4db5201aaae0
Michael Hoy <mjh@mjhoy.com>
2015-10-17 12:22:04 -0500

    gnu license

--- matsys-theme.el
+++ matsys-theme.el
@@ -1,5 +1,25 @@
 ;;; matsys-theme.el --- Fork of leuven
 
+;; This file is not part of GNU Emacs.
+
+;; The original levuen theme is authored by Fabrice Niessen and is
+;; available here: https://github.com/fniessen/emacs-leuven-theme
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Requirements: Emacs 24.
